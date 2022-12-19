1d7b58fde572960401357ce2d8eb5c2ef6ddfd6b
SilonCrutch <cfielda@gmail.com>
2022-12-19 18:24:57 +0800

    added variable

--- meme_flask/meme_flask.py
+++ meme_flask/meme_flask.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template
 import requests
 import json
 
+app = Flask(__name__)
+
 def get_meme():
     #Uncomment these two lines and comment out the other url line if you want to use a specific meme subreddt
     # sr = "/wholesomememes"
